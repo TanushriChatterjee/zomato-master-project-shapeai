e049b71f590950cbb9acb01c6f67566534c58a1f
TanushriChatterjee <tanushri1595@gmail.com>
2021-07-28 11:40:02 +0530

    add signin route

--- server/index.js
+++ server/index.js
@@ -1,15 +1,34 @@
+// Importing Env Variables
+require("dotenv").config();
+
+// Libraries
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 
-const zomato=express();
+// microservice routes
+import Auth from "./API/Auth";
 
-//middlewares
+// Database connection
+import ConnectDB from "./database/connection";
+
+const zomato = express();
+
+// application middlewares
 zomato.use(express.json());
-zomato.use(express.urlencoded({extended:false}));
+zomato.use(express.urlencoded({ extended: false }));
 zomato.use(helmet());
 zomato.use(cors());
 
-zomato.get("/",(req,res)=>res.json({message:"setup success"}));
+// Application Routes
+zomato.use("/auth", Auth);
 
-zomato.listen(4000,()=>console.log("Server is running 😎"));
+zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
+
+zomato.listen(4000, () =>
+  ConnectDB()
+    .then(() => console.log("Server is running 🚀"))
+    .catch(() =>
+      console.log("Server is running, but database connection failed... ")
+    )
+);
